32732491dead5b40dd5ef557ca8901cba13c2bd9
ryanloftus <ryanloftus02@outlook.com>
2023-03-21 19:37:42 -0400

    got app running and added basic login screen layout

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ import {
     Text,
     useColorScheme,
     View,
+    TextInput,
+    Button,
 } from 'react-native';
 
 import {
@@ -17,32 +19,19 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<
+const Login: React.FC<
     PropsWithChildren<{
-        title: string;
+        onLogin: any;
     }>
-> = ({ children, title }) => {
+> = ({ children, onLogin }) => {
     const isDarkMode = useColorScheme() === 'dark';
     return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}>
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
-                ]}>
-                {children}
-            </Text>
+        <View>
+            <Text>Login</Text>
+            <TextInput placeholder='Username'></TextInput>
+            <Text>Password</Text>
+            <TextInput placeholder='Password' secureTextEntry={true}></TextInput>
+            <Button title='Login' onPress={onLogin}></Button>
         </View>
     );
 };
@@ -56,34 +45,7 @@ const App = () => {
 
     return (
         <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={backgroundStyle}>
-                <Header />
-                <View
-                    style={{
-                        backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                    }}>
-                    <Section title="Step One">
-                        Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                        screen and then come back to see your edits.
-                    </Section>
-                    <Section title="See Your Changes">
-                        <ReloadInstructions />
-                    </Section>
-                    <Section title="Debug">
-                        <DebugInstructions />
-                    </Section>
-                    <Section title="Learn More">
-                        Read the docs to discover what to do next:
-                    </Section>
-                    <LearnMoreLinks />
-                </View>
-            </ScrollView>
+            <Login></Login>
         </SafeAreaView>
     );
 };
